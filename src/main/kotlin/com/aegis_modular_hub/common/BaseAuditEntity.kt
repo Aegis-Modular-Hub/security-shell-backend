129943d31a3f59d9fcad961bcb022cc537f10683
Jorge M <jorge.cervantes@et.bo>
2026-04-15 21:54:57 -0400

    generic audit data

---
+++ src/main/kotlin/com/aegis_modular_hub/common/BaseAuditEntity.kt
@@ -0,0 +1,32 @@
+package com.aegis_modular_hub.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseAuditEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    var createdBy: String? = null
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    var updatedBy: String? = null
+}
