97a957206249319d875ddbc0feef72a8cd1564b5
Jorge M <jorge.cervantes@et.bo>
2026-04-22 17:01:03 -0400

    uuid updated to id long

--- src/main/kotlin/com/aegis_modular_hub/appAuth/data/repository/RolRepository.kt
+++ src/main/kotlin/com/aegis_modular_hub/appAuth/data/repository/RolRepository.kt
@@ -1,6 +1,15 @@
 package com.aegis_modular_hub.appAuth.data.repository
 
 import com.aegis_modular_hub.appAuth.domain.entity.Rol
-import java.util.UUID
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-interface RolRepository : GenericRepository<Rol, UUID>
+interface RolRepository : GenericRepository<Rol, Long> {
+    fun existsByName(name: String): Boolean
+    @Query("""
+    SELECT COUNT(u) > 0
+    FROM UserAccount u JOIN u.roles r
+    WHERE r.id = :roleId
+""")
+    fun isRoleAssignedToUsers(@Param("roleId") roleId: Long): Boolean
+}
